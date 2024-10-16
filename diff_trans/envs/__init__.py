24ed9042c13fa47c62131a4f8dea4fa4f47149ff
tom1484 <tomchen2003611@gmail.com>
2024-10-16 14:48:42 +0800

    remove inverted pendulum v2

--- diff_trans/envs/__init__.py
+++ diff_trans/envs/__init__.py
@@ -1,7 +1,9 @@
 from .base import EnvConfig
 
 from .inverted_pendulum_v1 import InvertedPendulumConfig_v1
-from .inverted_pendulum_v2 import InvertedPendulumConfig_v2
+# from .inverted_pendulum_v2 import InvertedPendulumConfig_v2
 
 from .half_cheetah_v1 import HalfCheetahConfig_v1
 from .half_cheetah_v2 import HalfCheetahConfig_v2
+
+from .reacher_v1 import ReacherConfig_v1
--- diff_trans/envs/wrapped/__init__.py
+++ diff_trans/envs/wrapped/__init__.py
@@ -2,11 +2,11 @@ from .base import BaseEnv
 from .utils.entry import register, get_env
 
 from .inverted_pendulum_v1 import InvertedPendulum_v1
-from .inverted_pendulum_v2 import InvertedPendulum_v2
+# from .inverted_pendulum_v2 import InvertedPendulum_v2
 
 
 register("InvertedPendulum-v1", InvertedPendulum_v1)
-register("InvertedPendulum-v2", InvertedPendulum_v2)
+# register("InvertedPendulum-v2", InvertedPendulum_v2)
 
 
 from .half_cheetah_v1 import HalfCheetah_v1
